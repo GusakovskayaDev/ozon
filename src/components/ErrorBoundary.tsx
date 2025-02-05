19482efaea5d867438a4d01f9166cb7a1f31e2cb
PoniaPolla <gusakovskayadev@gmail.com>
2025-02-05 23:46:54 +0300

    Error Boundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import { Component, ReactNode } from "react";
+
+type ErrorBoundaryProps = {
+  children: ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Что-то пошло не так 😔</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
